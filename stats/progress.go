dd495b921dccd428ea55bcdc1ef12e84f7ce98d2
Steef de Rooi <s.derooi@ibfd.org>
2018-03-07 15:37:49 +0100

    refactoring

--- stats/progress.go
+++ stats/progress.go
@@ -6,15 +6,14 @@ import (
 
 // ProgressIndicator returns a function that displays progress as a percentage
 // of the total given. It only displays the progress at every 10% step.
-func ProgressIndicator(fileCount int) func() {
+func ProgressIndicator(total int) func() {
 	granularity := 10
-	total := fileCount
-	count := 0
+	onePercent := float64(total) / 100.0
+	done := 0
 	next := granularity
 	return func() {
-		count += 1
-		todo := float64(total - count)
-		perc := int(100.0 - (todo / (float64(total) / 100.0)))
+		done += 1
+		perc := int(float64(done) / onePercent)
 		if perc >= next {
 			next += granularity
 			fmt.Println(fmt.Sprintf("%d%% done", perc))
