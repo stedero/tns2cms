0f06708dc6c7b9a6c3baff11602a28b2a499c2ce
Steef de Rooi <s.derooi@ibfd.org>
2018-03-08 13:35:50 +0100

    added test code

--- naming/filenamer_test.go
+++ naming/filenamer_test.go
@@ -1,9 +1,37 @@
 package naming
 
 import (
+	"path/filepath"
 	"testing"
+	"os"
 )
 
-func TestInputFilename(t *testing.T) {
+const checkMark = "\u2713"
+const ballotX = "\u2717"
 
+func TestInputFilename(t *testing.T) {
+	t.Log("Given the need to test filenamer functionality")
+	file, _ := os.Stat("filenamer_test.go")
+	fileNamer := &Filenamer{"/test/indir", "/test/outdir", file}
+
+	expected := "/test/indir/filenamer_test.go"
+	actual := filepath.ToSlash(fileNamer.InputFilename())
+	assert(t, expected, actual, "InputFilename for \"filenamer_test.go\"")
+
+	expected = "/test/outdir/filenamer_test.go"
+	actual = filepath.ToSlash(fileNamer.OutputFilename())
+	assert(t, expected, actual, "OutputFilename for \"filenamer_test.go\"")
+
+	expected =  "/test/outdir/filenamer_test.metadata.properties.xml"
+	actual = filepath.ToSlash(fileNamer.MetaFilename())
+	assert(t, expected, actual, "MetaFilename for \"filenamer_test.go\"")
+}
+
+func assert(t *testing.T, expected string, actual string, what string) {
+	t.Logf("\tWhen checking %s", what)
+	if (actual == expected) {
+		t.Logf("\t\tIt should be \"%s\" %s", actual, checkMark)
+	} else {
+		t.Fatalf("\t\tIt should be \"%s\" but it was \"%s\" %s", expected, actual, ballotX)
+	}
 }
