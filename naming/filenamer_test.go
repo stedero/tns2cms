c7e7c695785ed4674ee403fd4aa7a4c61239fb09
Steef de Rooi <steef@Bakvis.local>
2018-03-09 13:45:07 +0100

    formatting

--- naming/filenamer_test.go
+++ naming/filenamer_test.go
@@ -1,9 +1,9 @@
 package naming
 
 import (
+	"os"
 	"path/filepath"
 	"testing"
-	"os"
 )
 
 const checkMark = "\u2713"
@@ -22,14 +22,14 @@ func TestInputFilename(t *testing.T) {
 	actual = filepath.ToSlash(fileNamer.OutputFilename())
 	assert(t, expected, actual, "OutputFilename for \"filenamer_test.go\"")
 
-	expected =  "/test/outdir/filenamer_test.metadata.properties.xml"
+	expected = "/test/outdir/filenamer_test.metadata.properties.xml"
 	actual = filepath.ToSlash(fileNamer.MetaFilename())
 	assert(t, expected, actual, "MetaFilename for \"filenamer_test.go\"")
 }
 
 func assert(t *testing.T, expected string, actual string, what string) {
 	t.Logf("\tWhen checking %s", what)
-	if (actual == expected) {
+	if actual == expected {
 		t.Logf("\t\tIt should be \"%s\" %s", actual, checkMark)
 	} else {
 		t.Fatalf("\t\tIt should be \"%s\" but it was \"%s\" %s", expected, actual, ballotX)
