dbacf9edf0f61bc80675044552b8b7250afd5c1c
Steef de Rooi <steef@Smartie.fritz.box>
2018-03-18 21:38:41 +0100

    refactoring

--- exec/visitor.go
+++ exec/visitor.go
@@ -30,9 +30,9 @@ func (visitor *Visitor) walker() func(string, os.FileInfo, error) error {
 	rootDirNamer := visitor.rootDirNamer
 	reporter := visitor.reporter
 	return func(path string, fileInfo os.FileInfo, err error) error {
-		validation := paths.Validate(fileInfo)
-		defer reporter.Register(validation, path)
-		switch validation {
+		action := paths.Validate(fileInfo)
+		defer reporter.Register(action, path)
+		switch action {
 		case paths.AcceptFile:
 			fileNamer := paths.NewFilenamerFromRoot(rootDirNamer, path, fileInfo)
 			visitor.process(fileNamer)
