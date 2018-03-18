9585d82e1de5a016b514591f2628b77c8564bbd5
Steef de Rooi <steef@Smartie.fritz.box>
2018-03-18 21:37:55 +0100

    more stats

--- stats/progress.go
+++ stats/progress.go
@@ -8,29 +8,27 @@ import (
 
 // Reporter definition
 type Reporter struct {
-	start               time.Time
-	directoriesAccepted int
-	filesAccepted       int
+	start  time.Time
+	counts []int
 }
 
 // NewReporter creates a reporter.
 func NewReporter() *Reporter {
-	return &Reporter{time.Now(), 0, 0}
+	return &Reporter{time.Now(), make([]int, paths.NumActions)}
 }
 
 // End reports the termination of the process with counts.
 func (reporter *Reporter) End() {
 	elapsed := time.Since(reporter.start)
-	log.Printf("processing %d files in %d directories took %s\n", reporter.filesAccepted, reporter.directoriesAccepted, elapsed)
+	log.Printf("skipped %d directories", reporter.counts[paths.RejectDir])
+	log.Printf("skipped %d files", reporter.counts[paths.RejectFile])
+	log.Printf("processing %d files in %d directories took %s", reporter.counts[paths.AcceptFile], reporter.counts[paths.AcceptDir], elapsed)
 }
 
 // Register a validation entry.
-func (reporter *Reporter) Register(validation paths.Validation, path string) {
-	switch validation {
-	case paths.AcceptDir:
-		reporter.directoriesAccepted++
-	case paths.AcceptFile:
-		reporter.filesAccepted++
+func (reporter *Reporter) Register(action paths.Action, path string) {
+	reporter.counts[action]++
+	switch action {
 	case paths.RejectDir:
 		log.Printf("skipped directory: %s\n", path)
 	case paths.RejectFile:
